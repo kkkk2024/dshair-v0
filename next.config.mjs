b1d8789da2a7c05a03e9c798edff798793106cc1
Caro <caro@dshairbeauty.co.uk>
2026-03-17 11:09:13 +0800

    fix: simplify image config, use local images

--- next.config.mjs
+++ next.config.mjs
@@ -3,22 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
 }
 
 export default nextConfig
